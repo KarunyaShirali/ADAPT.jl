fa0d60c63e391f09c8ab3aa58e26e152fadacc9f
Karunya Shirali <110246355+KarunyaShirali@users.noreply.github.com>
2024-06-19 18:32:28 -0400

    Update reconstruct_ansatz.jl

--- reconstruct_ansatz.jl
+++ reconstruct_ansatz.jl
@@ -8,27 +8,26 @@ import LinearAlgebra: norm
 n = 6
 
 # READ IN HAMILTONIAN
-# Hamiltonian_file = "qaoa_dataset/Hamiltonian1_n_6_Hamiltonian.csv"
 serialized_H = "qaoa_dataset/Hamiltonian1_n_"*string(n)*"_Hamiltonian"
 H = Serialization.deserialize(serialized_H)
 
 # READ IN ADAPT-QAOA RESULTS
 results_file = "qaoa_dataset/Hamiltonian1_n_"*string(n)*"_adaptqaoa_results.csv"
 csv = CSV.File(results_file); my_df = DataFrames.DataFrame(csv)
-
-# RECONSTRUCT ANSATZ
 gd = groupby(my_df, :run)
 
+# INITIALIZE THE ANSATZ 
 run = 1 # the index of the run for which you want to reconstruct the ansatz
 ψ0 = ones(ComplexF64, 2^n) / sqrt(2^n); ψ0 /= norm(ψ0) # initialize ψ0
 ansatz = ADAPT.ADAPT_QAOA.QAOAAnsatz(0.1, H) # initialize ansatz
 
-# BUILD OUT THE POOL
+# BUILD OUT THE OPERATOR POOL
 pooltype = gd[run][1,:pooltype]; 
 if pooltype=="qaoa_double_pool" 
     pool = ADAPT.ADAPT_QAOA.QAOApools.qaoa_double_pool(n)
 end
 
+# RECONSTRUCT ANSATZ
 for row in eachrow(gd[run])
     push!(ansatz, pool[row.:generator_index_in_pool] => 0.0)
 end
